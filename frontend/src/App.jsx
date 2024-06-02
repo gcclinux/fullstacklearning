1c01ebb1473560f026fe884891c7366de5afb1e5
Ricardo Wagemaker <java@wagemaker.co.uk>
2024-06-02 21:28:57 +0100

    added initial search componet

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header.jsx';
+import Search from './components/Search.jsx';
 
 const App = () =>{
    return (
       <div>
         <Header title="Images Galary"/>
+        <Search />
       </div>
   )
 }
