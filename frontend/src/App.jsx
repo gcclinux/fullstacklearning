3c08f3a177a249a0cc8d25636bf17762366abc73
Ricardo Wagemaker <java@wagemaker.co.uk>
2024-06-03 11:31:53 +0100

    Controlled search component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,19 @@
+import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header.jsx';
 import Search from './components/Search.jsx';
 
 const App = () =>{
+  const [word, setWord] = useState('');
+  const handleSearchSubmit = (e) => {
+    e.preventDefault();
+    console.log(word);
+  }
+
    return (
       <div>
         <Header title="Images Galary"/>
-        <Search />
+        <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit}/>
       </div>
   )
 }
--- frontend/src/components/Search.jsx
+++ frontend/src/components/Search.jsx
@@ -1,15 +1,20 @@
 import React from "react";
 import { Col, Row, Container, Form, Button } from "react-bootstrap";
 
-const Search = () => {
+const Search = ({ word, setWord, handleSubmit }) => {
     return (
         <Container className="mt-4">
             <Row className="justify-content-center">
                 <Col xs={12} md={8} lg={6}>
-                <Form>
+                <Form onSubmit={handleSubmit}>
                     <Form.Row>
                         <Col xs={9}>
-                            <Form.Control placeholder="Search for images" />
+                            <Form.Control 
+                                type="text"
+                                value={word}
+                                onChange={(e) => setWord(e.target.value)}
+                                placeholder="Search for images" 
+                            />
                         </Col>
                         <Col>
                             <Button variant="primary" type="submit">Search</Button>
