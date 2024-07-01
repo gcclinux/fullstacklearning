08b79081471f2a30486a5805eb1e042efdf774c9
Ricardo Wagemaker <java@wagemaker.co.uk>
2024-07-01 22:36:14 +0100

    edit text logo

--- README.md
+++ README.md
@@ -33,6 +33,7 @@ $ npm run dev
  - Markdown Preview Enhanced
  - Peacock
  - Prettier - Code formatter
+ - SVG Editor
 
 ###  VSCode formatting through the following shortcuts:
 
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,15 +1,16 @@
 import { Navbar, Container } from "react-bootstrap";
+import  Logo from "../images/logo.svg";
 
 const Header = (props) => {
   // eslint-disable-next-line
   const { title } = props;
   const navbarStyle = {
-    backgroundColor: "lightblue",
+    backgroundColor: "lightgray",
   };
   return (
     <Navbar style={navbarStyle} variant="light">
       <Container>
-        <Navbar.Brand href="/">{title}</Navbar.Brand>
+      <img style={{maxWidth: '14rem', maxHeight: '2rem'}} src={Logo} alt="Logo" />
       </Container>
     </Navbar>
   );
